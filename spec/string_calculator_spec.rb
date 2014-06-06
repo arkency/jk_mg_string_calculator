27fee7adad914e1077ce53fb1b672021115a84e7
Jakub Kosinski <jakub.kosinski@ghandal.net>
2014-06-06 15:22:54 +0200

    Test case for one number provided

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,4 +7,8 @@ describe StringCalculator do
   it 'returns 0 if empty string provided' do
     expect(subject.add("")).to eq(0)
   end
+
+  it 'returns number if one number provided' do
+    expect(subject.add("1")).to eq(1)
+  end
 end
